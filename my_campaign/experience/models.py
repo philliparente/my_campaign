b346ccfb66385f782a0f8642d9f281074d7278a5
Antonio Ladeia <antonioladeia@ideiaseo.com>
2015-08-11 13:08:51 -0300

    add descrição dos models

--- my_campaign/experience/models.py
+++ my_campaign/experience/models.py
@@ -3,9 +3,16 @@ from django.db import models
 class Character(models.Model):
     character_name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.character_name
+
+
 class Session(models.Model):
     session_title = models.CharField(max_length=200)
     session_date = models.DateTimeField()
     session_characters = models.ForeignKey(Character)
+
+    def __str__(self):
+	return self.session_title
 
 # Create your models here.
