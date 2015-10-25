0ca85ea38d00e70802af68a1726c1786fa68e2de
Raphael Fruneaux <raphaelfruneaux@Raphael.local>
2015-10-24 22:47:12 -0300

    ajustes no método de login e logout

--- experience/views.py
+++ experience/views.py
@@ -5,11 +5,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Campaign
 
 from django.contrib.auth.decorators import login_required
@@ -21,10 +22,10 @@ def index(request):
 
 @login_required
 def campaigns(request):
-
     if request.POST and request.POST['username']:
-        user = User.objects.filter(username=username)
-        campaigns = Campaign.objects.filter(master=user)
+
+        user = User.objects.filter(username=request.POST['username'])
+        campaigns = Campaign.objects.filter(author=user)
 
         return render(request, 'experience/campaigns.html', {'campaigns': campaigns})
 
@@ -40,53 +41,55 @@ def campaign(request, campaign_id):
 def contributors(request):
     return render(request, 'experience/contributors.html')
 
+
 def auth_login(request):
+    message = ''
 
-	message = ''
-	if request.POST:
-		user  = request.POST
-		print user['username']
-		is_valid = authenticate(username=user['username'], password=user['password'])
+    if request.user.is_authenticated():
+        return render(request, 'experience/mypage.html', {})
 
-		if is_valid is not None:
-		    # the password verified for the user
-		    if is_valid.is_active:
-		    	login(request, is_valid)
-		        message = "User is valid, active and authenticated"
-		    else:
-		        message = "The password is valid, but the account has been disabled!"
-		else:
-		    # the authentication system was unable to verify the username and password
-		    message = "The username and password were incorrect."
+    if request.POST:
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
 
-		return render(request, 'experience/mypage.html', {'message':message})
-	return render(request, 'experience/login.html', {'message':message})
+        if user is not None:
+            # the password verified for the user
+            if user.is_active:
+                login(request, user)
+                message = "User is valid, active and authenticated"
+                return render(request, 'experience/mypage.html', {'message': message})
+            else:
+                message = "The password is valid, but the account has been disabled!"
+        else:
+            # the authentication system was unable to verify the username and password
+            message = "The username and password were incorrect."
+
+    return render(request, 'experience/login.html', {'message': message})
+
 
 def signup(request):
+    message = ''
+    if request.POST:
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
 
-	message = ''
-	if request.POST:
-		username = request.POST['username']
-		email = request.POST['email']
-		password  = request.POST['password']
-		first_name = request.POST['first_name']
-		last_name = request.POST['last_name']
+        try:
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=password,
+                first_name=first_name,
+                last_name=last_name
+            )
+            message = 'Usuário salvo com sucesso'
+        except:
+            message = 'Erro ao salvar usuário'
 
-		try:
-			User.objects.create_user(
-				username=username,
-				email=email,
-				password=password,
-				first_name=first_name,
-				last_name=last_name
-			)
-			message = 'Usuário salvo com sucesso'
-		except:
-			message = 'Erro ao salvar usuário'
-
-	return render(request, 'experience/signup.html', {'message':message})
+    return render(request, 'experience/signup.html', {'message': message})
 
 
-def logout(request):
+def auth_logout(request):
     logout(request)
-    #return render(request, 'experience/index.html')
+    return redirect(reverse("experience:index"))
